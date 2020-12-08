1f3974d57d986134682f4c75816a9e6ca65ba582
Joao pedro Ceia <jceia@c1r1s1.42lisboa.com>
2020-12-08 08:24:54 +0000

    ft_boolean.h

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: jceia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 00:22:59 by jceia             #+#    #+#             */
-/*   Updated: 2020/12/07 00:32:17 by jceia            ###   ########.fr       */
+/*   Updated: 2020/12/08 07:28:45 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
 # define SUCCESS 0
 
 # define EVEN(x) x % 2 == 0 ? 1 : 0
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG  "I have an odd number of arguments."
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG  "I have an odd number of arguments.\n"
 
 typedef int	t_bool;
 
